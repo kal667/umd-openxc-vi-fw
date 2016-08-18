7a078ee3f2c1a0acfe5176df134e24c1b5b4ce25
kal667 <kkelly.667@gmail.com>
2016-08-18 12:11:44 -0400

    Moved gear from json to handlers. Debugged so only error now invloves the composite write for the infotainment. Still have not tested.

--- my_handlers.cpp
+++ my_handlers.cpp
@@ -4,7 +4,7 @@ namespace can = openxc::can;
 
 using openxc::can::lookupSignal;
 
-//Translate decimal RPM value to hex
+//Encodes decimal RPM value
 uint64_t ourRPMWriteEncoder(CanSignal* signal, openxc_DynamicField* value, bool* send) {
 		
 	*send = true;
@@ -18,7 +18,7 @@ uint64_t ourRPMWriteEncoder(CanSignal* signal, openxc_DynamicField* value, bool*
 	return can_value;
 }
 
-//Translate decimal MPH value to hex
+//Encodes decimal MPH value
 uint64_t ourMPHWriteEncoder(CanSignal* signal, openxc_DynamicField* value, bool* send) {
 		
 	*send = true;
@@ -33,7 +33,7 @@ uint64_t ourMPHWriteEncoder(CanSignal* signal, openxc_DynamicField* value, bool*
 	return can_value;
 }
 
-//Translates percentage of heat gauge to hex
+//Encodes percentage of heat gauge.
 uint64_t ourEngineTempEncoder(CanSignal* signal, openxc_DynamicField* value, bool* send) {
 	
 	*send = true;
@@ -47,7 +47,7 @@ uint64_t ourEngineTempEncoder(CanSignal* signal, openxc_DynamicField* value, boo
 	return can_value;
 }
 
-//Translates boolean to hex
+//Encodes boolean awake status. Put this command in a while loop to run in Terminal.
 uint64_t keepAwakeBooleanEncoder(CanSignal* signal, openxc_DynamicField* value, bool* send) {
 
 	*send = true;
@@ -55,12 +55,12 @@ uint64_t keepAwakeBooleanEncoder(CanSignal* signal, openxc_DynamicField* value, 
 	uint64_t can_value = 0;
 	bool temp = value->boolean_value;
 
-	if (temp == true) can_value = 0x45;
+	if (temp == true) can_value = 0x45;//Keep Awake On
 
 	return can_value;
 }
 
-//Translates boolean to hex
+//Encodes boolean light status.
 uint64_t lightsBooleanEncoder(CanSignal* signal, openxc_DynamicField* value, bool* send) {
 
 	*send = true;
@@ -68,9 +68,69 @@ uint64_t lightsBooleanEncoder(CanSignal* signal, openxc_DynamicField* value, boo
 	uint64_t can_value = 0;
 	bool temp = value->boolean_value;
 
-	if (temp == true) can_value = 0x000100000001410000;
-	if (temp == false) can_value = 0x000000000001010000;
+	if (temp == true) can_value = 0xFFFF011200;//Lights ON
+	if (temp == false) can_value = 0xFFFF010C40;//Lights OFF
 
 	return can_value;
 }
+
+//Encodes string gear status.
+uint64_t gearEncoder(CanSignal* signal, openxc_DynamicField* value, bool* send) {
+
+	*send = true;
+	
+	uint64_t can_value = 0;
+	const char* gear = value->string_value;
+
+	if (!strcmp("R", gear)) can_value = 0xA100000000005E30;//Reverse
+	if (!strcmp("N", gear)) can_value = 0xEE00000000005E50;//Neutral
+	if (!strcmp("D", gear)) can_value = 0x1700000000005E70;//Drive
+	if (!strcmp("M", gear)) can_value = 0x1700000000005ED2;//Manual
+
+	return can_value;
+}
+
+//Handler to control infotainment
+void infotainmentHandler(const char* name, openxc_DynamicField* value,
+	openxc_DynamicField* event, CanSignal* signals, int signalCount) {
+	
+	CanMessage message = {0};
+	openxc_DynamicField* can_value = 0;
+
+	const char* button = value->string_value;//this probably isn't right
+
+	CanSignal* buttonSignal = lookupSignal("infotainment_button", signals,
+		signalCount);
+
+	CanBus* bus = openxc::can::lookupBus(2, openxc::signals::getCanBuses(), openxc::signals::getCanBusCount());
+
+	if (!strcmp("v_up", button)) {
+		can_value = 11144;//0x2B88;
+		message = {0x2E0, STANDARD, {0x2B, 0x48}, 2};
+	}
+
+	if (!strcmp("v_down", button)) {
+		can_value = 11400;//0x2C88;
+		message = {0x2E0, STANDARD, {0x2C, 0x48}, 2};
+	}
+
+	if (!strcmp("t_up", button)) {
+		can_value = 10632;//0x2988;
+		message = {0x2E0, STANDARD, {0x29, 0x48}, 2};
+	}
+
+	if (!strcmp("t_down", button)) {
+		can_value = 10888;//0x2A88;
+		message = {0x2E0, STANDARD, {0x2A, 0x48}, 2};
+	}
+
+	if (!strcmp("t_toggle", button)) {
+		can_value = 32904;//0x8088;
+		message = {0x2E0, STANDARD, {0x80, 0x48}, 2};
+	}
+
+	can::write::enqueueMessage(bus, &message);
+	can::write::encodeAndSendSignal(buttonSignal, can_value, true);
+}
+
 
